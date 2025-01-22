ecfb12308e9d571734dae806d799361e237b43ab
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2025-01-22 18:07:00 -0500

    Update script.jl

--- script.jl
+++ script.jl
@@ -3,7 +3,7 @@ using LinearAlgebra
 using Plots
 using SparseArrays
 
-vals = [2,7]
+vals = [0,7]
 A = diagm(vals)
 b = zeros(2)
 f(x,y) = 0.5*[x,y]'*A*[x,y] - b'*[x,y]
@@ -12,9 +12,9 @@ xs = range(-1, 1, length = 100)
 ys = range(-1, 1, length = 100)
 
 z = f.(xs', ys)
-plt = contour(xs, y, z; fill=true, color=:turbo, levels=25)
+plt = contour(xs, ys, z; fill=true, color=:turbo, levels=25)
 
-x0 = [-0.5, 0.1]
+x0 = [-0.5, 0]
 
 r0 = b - A*x0
 
@@ -24,7 +24,7 @@ maxiter = 10
 res_xs = [x0[1]]
 res_ys = [x0[2]]
 
-for iter in 1:maxiter 
+for iter in 1:maxiter
   alpha = dot(r, r)/dot(r, A*r)
   x .+= alpha .* r
   push!(res_xs, x[1])
@@ -43,7 +43,13 @@ A = diagm(range(4.99, 5.01, length=20))
 cg(A, b)
 
 A = diagm(range(5, 100, length=20))
-cg(A, b)
+x, stats = cg(A, b)
+
+x[1] += 100
+x0 = x
+
+x, stats = cg(A, b, x0)
+
 
 A = diagm(range(99.99, 100.01, length=20))
 cg(A, b)
