802ad8effc45f65c5f5acc9bfa2ff65c7a9fa559
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2025-01-21 19:44:43 -0500

    Make contour plot

--- script.jl
+++ script.jl
@@ -5,20 +5,48 @@ using Plots
 
 vals = [2,7]
 A = diagm(vals)
+b = zeros(2)
+f(x,y) = 0.5*[x,y]'*A*[x,y] - b'*[x,y]
 
-scatter(vals, [0,0])
+xs = range(-1, 1, length = 100)
+ys = range(-1, 1, length = 100)
 
-function get_ith_Tpoly(i)
-  coeffs = zeros(i+1)
-  coeffs[end] = 1
-  return Polynomials.ChebyshevT(coeffs)
+z = f.(xs', ys)
+plt = contour(xs, y, z; fill=true, color=:turbo, levels=25)
+
+x0 = [-0.5, 0.1]
+
+r0 = b - A*x0
+
+r = r0
+x = x0
+maxiter = 10
+res_xs = [x0[1]]
+res_ys = [x0[2]]
+
+for iter in 1:maxiter 
+  alpha = dot(r, r)/dot(r, A*r)
+  x .+= alpha .* r
+  push!(res_xs, x[1])
+  push!(res_ys, x[2])
+
+  r .= b .- A * x
 end
+scatter!(res_xs, res_ys)
 
-T = get_ith_Tpoly(2)
+# scatter(vals, [0,0])
 
-xs = range(-0.5, 7.5, length=500)
+# function get_ith_Tpoly(i)
+#   coeffs = zeros(i+1)
+#   coeffs[end] = 1
+#   return Polynomials.ChebyshevT(coeffs)
+# end
 
-top_input = (7 .+ 2 .- 2 .* xs) ./ (7 .- 2)
-denom = Polynomials.evalpoly((7 + 2) / (7 - 2), T, false)
+# T = get_ith_Tpoly(2)
 
-plot!(xs, Polynomials.evalpoly.(top_input, T, false) ./ denom)
+# xs = range(-0.5, 7.5, length=500)
+
+# top_input = (7 .+ 2 .- 2 .* xs) ./ (7 .- 2)
+# denom = Polynomials.evalpoly((7 + 2) / (7 - 2), T, false)
+
+# plot!(xs, Polynomials.evalpoly.(top_input, T, false) ./ denom)
